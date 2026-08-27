36fddbbc2e70695a44ca71f0c765bc065d8a87d5
gohjiemin1010 <gohjiemin1010@gmail.com>
2026-08-27 18:49:11 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import xgboost as xgb
+import time
+from scipy.stats import norm
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+
 # ==========================================
 # 1. Page Configuration & Custom CSS 
 # ==========================================
